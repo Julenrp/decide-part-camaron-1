736a1d78861837e716ad5e5b398d188ccec4265c
pedlopruz <plopezr2011@gmail.com>
2023-11-12 15:30:00 +0100

    Fix Error en la creacion de peticion:
    Confusión a la hora de crear otra aplicación ya que el codigo era para el censo

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
-    path('peticion/', include('peticion.urls')),
+    path('census/', include('census.urls')),
 ]
 
 for module in settings.MODULES:
