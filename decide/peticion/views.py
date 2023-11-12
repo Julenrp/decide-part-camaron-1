a58adaed7aa6b022731acf5b6d69476cac72e088
manortbla <manortbla@alum.us.es>
2023-11-12 12:15:15 +0100

    Feat: Creacion de vista para  metodo de peticion:
    Se añade el método dentro del views.py para la
    petición del usuario de ser añadido al censo.

--- decide/peticion/views.py
+++ decide/peticion/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FormularioPeticion
+from django.core.mail import EmailMessage
 
 # Create your views here.
+def contacto(request):
+    formulario_Peticion = FormularioPeticion()
+
+    if request.method == "POST":
+        formulario_Peticion = FormularioPeticion(data=request.POST)
+        if formulario_Peticion.is_valid():
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+            email2 = EmailMessage("Peticion de censo","El usuario con nombre {} y correo {} solicita:\n\n{}"
+                                  .format(nombre, email, contenido),"",["nanomotors33@gmail.com"],reply_to=[email])
+            try:
+                email2.send()
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")
+
+    return render(request, "contacto/contacto.html", {"miFormulario":formulario_Peticion})
+
