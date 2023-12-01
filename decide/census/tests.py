98f8b7ff9dcf3dafdb2d0da6e78c0a198f310f2a
Julenrp <julenfabi@gmail.com>
2023-12-01 13:25:03 +0100

    Feat tests importar censo a json:
    Testeo del correcto funcionamiento de la importación a json del censo
    
    Co-authored-by: manbarjim2 <manbarjim2@users.noreply.github.com>

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -1,6 +1,9 @@
-import random
+import json
+import tempfile
+import os
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework.test import APIClient
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
@@ -13,8 +16,28 @@ from selenium.webdriver.common.keys import Keys
 from .models import Census
 from base import mods
 from base.tests import BaseTestCase
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
+class BaseExportTestCase(TestCase):
+    def setUp(self):
+        #Creamos 4 censos distintos
+        self.census_data = [
+            {
+                'voting_id': i,
+                'voter_id': i,
+            }
+            for i in range(1, 5) 
+        ]
+        #Creamos los censos con datos de census_data
+        self.census_create = [Census.objects.create(**data) for data in self.census_data]
+        
+        #Verificamos que cada censo creado, verifica que voter_id y voting_id son i+1
+        for i, censo in enumerate(self.census_create):
+            self.assertEqual(censo.voting_id, i + 1)
+            self.assertEqual(censo.voter_id, i + 1)
+        
+    def tearDown(self):
+        Census.objects.all().delete()
 
 class CensusTestCase(BaseTestCase):
 
@@ -164,3 +187,50 @@ class CensusTest(StaticLiveServerTestCase):
 
         self.assertTrue(self.cleaner.find_element_by_xpath('/html/body/div/div[3]/div/div[1]/div/form/div/p').text == 'Please correct the errors below.')
         self.assertTrue(self.cleaner.current_url == self.live_server_url+"/admin/census/census/add")
+
+class ExportCensusJSONTest(BaseExportTestCase):
+    def testExportListJson(self):
+        url_exportacion = reverse('export_census_json')
+        response = self.client.get(url_exportacion)
+        self.assertEqual(response.status_code, 200)
+
+        datos_exportados = json.loads(response.content.decode('utf-8'))
+
+        self.assertIsInstance(datos_exportados, list)
+        self.assertEqual(len(datos_exportados), len(self.census_create))
+
+        # Iteramos a través de cada elemento exportado y comparamos con la instancia de censo correspondiente
+        for indice, datos_censo_exportado in enumerate(datos_exportados):
+            if indice < len(self.census_create):
+                self.assertCheckCreatedCensusDataEqualsCensusData(datos_censo_exportado, self.census_create[indice])
+
+    def testExportedJsonFile(self):
+        url_exportacion = reverse('export_census_json')
+        response = self.client.get(url_exportacion)
+        self.assertEqual(response.status_code, 200)
+
+        datos_respuesta = json.loads(response.content.decode('utf-8'))
+
+        # Creamos un archivo temporal con extensión .json para almacenar los datos exportados
+        with tempfile.NamedTemporaryFile(suffix='.json', delete=False, mode='w+b') as archivo_temporal:
+            archivo_temporal.write(json.dumps(datos_respuesta, indent=2, default=str).encode('utf-8'))
+            ruta_archivo_temporal = archivo_temporal.name
+
+        self.assertTrue(os.path.exists(ruta_archivo_temporal))
+
+        # Leemos los datos guardados desde el archivo JSON temporal
+        with open(ruta_archivo_temporal, 'r', encoding='utf-8') as archivo_json_temporal:
+            datos_guardados = json.load(archivo_json_temporal)
+
+        # Comparamos cada conjunto de datos guardados con las instancias del censo creado
+        for indice, datos_censo_guardado in enumerate(datos_guardados):
+            if indice < len(self.census_create):
+                self.assertCheckCreatedCensusDataEqualsCensusData(datos_censo_guardado, self.census_create[indice])
+
+        os.remove(ruta_archivo_temporal)
+
+    def assertCheckCreatedCensusDataEqualsCensusData(self, exported_data, census_create):
+        self.assertEqual(exported_data['voting_id'], census_create.voting_id)
+        self.assertEqual(exported_data['voter_id'], census_create.voter_id)
+        expected_keys = ['voting_id', 'voter_id']
+        self.assertCountEqual(exported_data.keys(), expected_keys)
