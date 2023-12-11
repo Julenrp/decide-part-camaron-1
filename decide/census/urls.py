e1019953ec2224715250b67f9157ccbfd85f5e8e
Julenrp <julenfabi@gmail.com>
2023-12-11 15:46:10 +0100

    Fix TEST DE EXPORTACION DE CENSOS Fixes #23

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -7,11 +7,9 @@ from .views import *
 urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
-
     path('export_csv/', ExportCensusCsv.as_view(), name='export_census_csv'),
-    path('export_json/', ExportCensusJson.as_view(), name='export_census_json')
-
-    path('peticion/', views.peticionCenso, name="peticion"),
+    path('export_json/', ExportCensusJson.as_view(), name='export_census_json'),
+    path('peticion/', views.peticionCenso, name="peticion")
     
 
 ]
