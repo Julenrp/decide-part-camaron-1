573fc5a48dbf1260c858bdce1839c909cf494cfa
pedlopruz <plopezr2011@gmail.com>
2023-12-16 17:08:40 +0100

    Fix probar

--- decide/census/forms.py
+++ decide/census/forms.py
@@ -2,8 +2,6 @@ from django import forms
 
 class FormularioPeticion(forms.Form):
 
-    census_id = forms.IntegerField(label="census_id")
     nombre = forms.CharField(label="Nombre", required=True)
     email = forms.EmailField(label="Email", required=True)
     contenido = forms.CharField(label="Contenido", widget=forms.Textarea, required=True)
-
