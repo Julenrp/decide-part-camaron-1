3c629c764a41185c5df2f5008306d8e553fc54cb
pedlopruz <plopezr2011@gmail.com>
2023-12-18 19:14:53 +0100

    Feat Vizualizar Votos:
    Se añadido una vista de visualizar votos

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import VisualizerView
+from .views import VisualizerView, listarVotacion
 
 
 urlpatterns = [
-    path('<int:voting_id>/', VisualizerView.as_view()),
+    path('<int:voting_id>/', VisualizerView.as_view(), name="Visualizar"),
+    path('verVotaciones/',listarVotacion,name="VerVotaciones")
 ]
